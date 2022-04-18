9746540f8fbe9ab39790fa762d28992d86f99d36
IronBankDev <dev@ib.xyz>
2022-04-18 16:56:35 +0800

    reindent deploy script

--- deploy/001_deploy_liquidity_mining.js
+++ deploy/001_deploy_liquidity_mining.js
@@ -1,35 +1,35 @@
 const { ethers } = require('hardhat');
 
 module.exports = async({
-    getNamedAccounts,
-    deployments,
-    getChainId,
-    getUnnamedAccounts,
+  getNamedAccounts,
+  deployments,
+  getChainId,
+  getUnnamedAccounts,
 }) => {
-    const {deploy, get} = deployments;
-    const {deployer, multisig} = await getNamedAccounts();
+  const {deploy, get} = deployments;
+  const {deployer, multisig} = await getNamedAccounts();
 
-    const lm = await deploy('LiquidityMining', {
-        from: deployer,
-        args: [],
-        log: true
-    });
+  const lm = await deploy('LiquidityMining', {
+    from: deployer,
+    args: [],
+    log: true
+  });
 
-    const comptroller = (await get('Comptroller')).address;
+  const comptroller = (await get('Comptroller')).address;
 
-    const liquidityMiningFactory = await ethers.getContractFactory("LiquidityMining");
-    const fragment = liquidityMiningFactory.interface.getFunction('initialize');
-    const initData = liquidityMiningFactory.interface.encodeFunctionData(fragment, [multisig, comptroller])
+  const liquidityMiningFactory = await ethers.getContractFactory("LiquidityMining");
+  const fragment = liquidityMiningFactory.interface.getFunction('initialize');
+  const initData = liquidityMiningFactory.interface.encodeFunctionData(fragment, [multisig, comptroller])
 
-    const proxy = await deploy('LiquidityMiningProxy', {
-        from: deployer,
-        args: [lm.address, initData],
-        log: true
-    });
+  const proxy = await deploy('LiquidityMiningProxy', {
+    from: deployer,
+    args: [lm.address, initData],
+    log: true
+  });
 
-    await deploy('LiquidityMiningLens', {
-        from: deployer,
-        args: [proxy.address],
-        log: true
-    })
+  await deploy('LiquidityMiningLens', {
+    from: deployer,
+    args: [proxy.address],
+    log: true
+  })
 }
